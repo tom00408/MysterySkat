89b87739794fde7a15b30fad307c99df27ea5dfb
tom00408 <tom.tiedtke@aol.com>
2025-07-16 16:43:03 +0200

    Initial Commit

---
+++ MysteryRamschen/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MysteryRamschen
+//
+//  Created by Tom Tiedtke on 16.07.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MysteryRamschen/MysteryRamschenApp.swift
@@ -0,0 +1,17 @@
+//
+//  MysteryRamschenApp.swift
+//  MysteryRamschen
+//
+//  Created by Tom Tiedtke on 16.07.25.
+//
+
+import SwiftUI
+
+@main
+struct MysteryRamschenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
