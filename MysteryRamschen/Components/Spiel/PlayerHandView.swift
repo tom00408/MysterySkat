bd5a1c0acc3567f9411b12adf5162735c2930127
tom00408 <tom.tiedtke@aol.com>
2025-07-18 01:12:58 +0200

    bilder

--- MysteryRamschen/Components/Spiel/PlayerHandView.swift
+++ MysteryRamschen/Components/Spiel/PlayerHandView.swift
@@ -8,10 +8,27 @@
 import SwiftUI
 
 struct PlayerHandView: View {
+    let cards = ["pik7", "pik8", "pik9", "pikT", "pikB"] // Platzhalter-Symbole
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-        Text("Test Moin Moin")
-        
+        ZStack {
+            ForEach(cards.indices, id: \.self) { index in
+                Button{
+                    print(cards[index])
+                }label:{
+                    Image(cards[index])
+                        .resizable()
+                        .font(.system(size: 60))
+                        .frame(width: 80, height: 120)
+                        .background(Color.white)
+                        .cornerRadius(10)
+                        .shadow(radius: 2)
+                         // ← Abstand der Karten
+                    
+                }.offset(x: CGFloat(index) * 30)
+            }
+        }
+        .frame(height: 150)
     }
 }
 
