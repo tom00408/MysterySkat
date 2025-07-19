d916e743351d7d42b9963dc20d8d419ff0778d82
tom00408 <tom.tiedtke@aol.com>
2025-07-19 14:56:30 +0200

    1

--- MysteryRamschen/Components/Spiel/KartenGridView.swift
+++ MysteryRamschen/Components/Spiel/KartenGridView.swift
@@ -1,68 +1,22 @@
 import SwiftUI
 
 struct KartenGridView: View {
-    let kartenBreite = 100
-    let kartenHoehe = 140
-    let karten: [Image]
-
-    init() {
-        self.karten = KartenGridView.loadImages()
-    }
-
-    var body: some View {
-        VStack {
-            ForEach(0..<4, id: \.self) { i in
-                HStack {
-                    ForEach(0..<8, id: \.self) { j in
-                        if i * 8 + j < karten.count {
-                            karten[i * 8 + j]
-                                .resizable()
-                                .frame(width: CGFloat(kartenBreite), height: CGFloat(kartenHoehe))
-                        }
-                    }
-                }
+ 
+    let karten : [String] = ["pik7","herzA"]
+    
+    var body: some View{
+        HStack{
+            
+            ForEach(karten, id:  \.self){ karte in
+                
+                Image(karte)
+                    .resizable()
+                    .frame(width: 130, height: 200)
             }
         }
     }
-
-    static func loadImages() -> [Image] {
-        var result: [Image] = []
-
-        guard let url = Bundle.main.url(forResource: "karten2", withExtension: "jpeg"),
-              let originalImage = UIImage(contentsOfFile: url.path)?.cgImage else {
-            print("Bild konnte nicht geladen werden")
-            return result
-        }
-
-        let kartenBreite = 100
-        let kartenHoehe = 140
-
-        let bildBreite = originalImage.width
-        let bildHoehe = originalImage.height
-
-        let eckeX = 0 // <- ggf. wieder 1 + 5 * kartenBreite, wenn Sprite groß genug ist
-        let eckeY = 0
-
-        for i in 0..<4 {
-            for j in 0..<8 {
-                let x = eckeX + j * kartenBreite
-                let y = eckeY + i * kartenHoehe
-
-                // Check, ob Ausschnitt im Bildbereich liegt
-                if x + kartenBreite <= bildBreite, y + kartenHoehe <= bildHoehe {
-                    let rect = CGRect(x: x, y: y, width: kartenBreite, height: kartenHoehe)
-                    if let cropped = originalImage.cropping(to: rect) {
-                        let image = Image(decorative: cropped, scale: 1.0)
-                        result.append(image)
-                    }
-                } else {
-                    print("⚠️ Überschreitet Bildgrenze bei Karte \(i), \(j) – wird übersprungen")
-                }
-            }
-        }
-
-        return result
-    }
+    
+    
 }
 
 
--- MysteryRamschen/Components/Spiel/PlayerHandView.swift
+++ MysteryRamschen/Components/Spiel/PlayerHandView.swift
@@ -8,26 +8,28 @@
 import SwiftUI
 
 struct PlayerHandView: View {
-    let cards = ["pik7", "pik8", "pik9", "pikT", "pikB"] // Platzhalter-Symbole
+    @State var cards = ["pik7", "pik8", "pik9", "pik10", "pikB","kreuzB","herzB", "kreuzA","kreuz7","kreuz8"]
     
     var body: some View {
         ZStack {
             ForEach(cards.indices, id: \.self) { index in
                 Button{
                     print(cards[index])
+                    //cards.remove(at: index)
+                    
                 }label:{
                     Image(cards[index])
                         .resizable()
                         .font(.system(size: 60))
-                        .frame(width: 80, height: 120)
+                        .frame(width: 120, height: 140)
                         .background(Color.white)
                         .cornerRadius(10)
                         .shadow(radius: 2)
-                         // ← Abstand der Karten
+                         
                     
-                }.offset(x: CGFloat(index) * 30)
+                }.offset(x: CGFloat(index) * 35)
             }
-        }
+        }.offset(x: -25 * CGFloat(cards.count) / 2)
         .frame(height: 150)
     }
 }
